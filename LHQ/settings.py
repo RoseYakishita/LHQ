cf01f35f5ab709344ae06ad3dcab244caee1dc60
RoseYakishita <anson33213@gmail.com>
2024-03-20 18:36:38 +0700

    Initial commit

--- LHQ/settings.py
+++ LHQ/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
