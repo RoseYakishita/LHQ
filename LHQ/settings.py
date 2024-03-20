348b51262368557406acb23d7b817585797ffe39
RoseYakishita <anson33213@gmail.com>
2024-03-20 16:39:38 +0700

    Initial commit

--- LHQ/settings.py
+++ LHQ/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o!59wg9%x@2+%8qt(o=%8xr0sq*d0rsut+bft5&h5lv-^+u#z+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
